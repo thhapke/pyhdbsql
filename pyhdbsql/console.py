27efc7b81e40bbe64ef50f99b8a1d3b9cdf4c712
thhapke <thorsten.hapke@sap.com>
2021-01-24 05:40:04 +0100

    update

--- pyhdbsql/console.py
+++ pyhdbsql/console.py
@@ -1,7 +1,6 @@
 
 
 import logging
-from argparse import ArgumentParser
 import sys
 import csv
 import re
@@ -26,6 +25,57 @@ def read_csv(filename) :
             records.append(line)
     return records
 
+### DB routines
+def upload2db(sql, data,db,batchsize = 0,infobatch = 0  ) :
+    conn = dbapi.connect(address=db['host'], port=db['port'], user=db['user'], password=db['pwd'], encrypt=True,
+                         sslValidateCertificate=False)
+
+    cursor = conn.cursor()
+
+    if batchsize == 0 :
+        logging.debug('Uploading: {}'.format(len(data)))
+        cursor.executemany(sql, data)
+    elif batchsize == 1 :
+        logging.debug('Uploading each record separately: {}'.format(len(data)))
+        for i,rec in enumerate(data) :
+            if infobatch> 0 and  i%infobatch == 0 :
+                logging.debug('Uploaded: {}/{}'.format(i,len(data)))
+            try :
+                cursor.execute(sql, rec)
+            except dbapi.DataError as de :
+                logging.warning('Data Error: line {} ({})'.format(i,de))
+                logging.warning('Record: {}'.format(rec))
+    else:
+        logging.debug('Uploading in batches: {} of batch size: {} (#{})'.format(len(data),batchsize,int(len(data)/batchsize)+1))
+        for i in range(0,len(data),batchsize) :
+            logging.debug('Uploaded: {}/{} - Uploading: {}'.format(i,len(data),len(data[i:i+batchsize])))
+            cursor.executemany(sql, data[i:i + batchsize])
+
+    cursor.close()
+    conn.close()
+
+def execute_select(db,sql) :
+    conn = dbapi.connect(address=db['host'], port=db['port'], user=db['user'], password=db['pwd'], encrypt=True,
+                         sslValidateCertificate=False)
+    cursor = conn.cursor()
+    ret = cursor.execute(sql)
+    logging.info('SQL: {}'.format(sql))
+    logging.info('DB response: {}'.format(ret))
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return rows
+
+def execute_sql(db,sql) :
+    conn = dbapi.connect(address=db['host'], port=db['port'], user=db['user'], password=db['pwd'], encrypt=True,
+                         sslValidateCertificate=False)
+    cursor = conn.cursor()
+    ret = cursor.execute(sql)
+    logging.info('SQL: {}'.format(sql))
+    logging.info('DB response: {}'.format(ret))
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
 
 def main() :
     ###
@@ -52,25 +102,19 @@ def main() :
 
     logging.info('Connection to db: {}'.format(conn))
 
-    dataupload = True if re.match('INSERT\s+.+\(\s*\?\s*,',sql) or re.match('UPSERT\s+.+\(\s*\?\s*,',sql) else False
+    sqlupload = True if re.match('INSERT\s+.+\(\s*\?\s*,',sql) or re.match('UPSERT\s+.+\(\s*\?\s*,',sql) else False
+    sqlselect = True if re.match('SELECT\s+', sql) else False
 
-    if dataupload :
+    if sqlupload :
         records = read_csv(params['INPUT_CSV'])
-        cursor = conn.cursor()
-        ret = cursor.executemany(sql,records)
-        logging.info('Data uploaded: {}'.format(ret))
-        cursor.close()
-        conn.close()
+        upload2db(sql=sql,data=records,db=db,batchsize=params['BATCHSIZE'],infobatch=params['INFO_BATCHSIZE'])
 
-    elif sql :
-        cursor = conn.cursor()
-        ret = cursor.execute(sql)
-        logging.info('SQL: {}'.format(sql))
-        logging.info('DB response: {}'.format(ret))
-        rows = cursor.fetchall()
-        write_csv(params['OUTPUT_FILE'],sql,rows)
-        cursor.close()
-        conn.close()
+    elif sqlselect :
+        data = execute_select(db=db,sql=sql)
+        write_csv(params['OUTPUT_FILE'], sql, data)
+
+    else:
+        execute_sql(db=db,sql=sql)
 
 if __name__ == '__main__':
     main()
